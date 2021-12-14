1b14f33cab3ccb6894b598d7bdf95bf81d2b204d
Alex Kulbach <savisaar2@gmail.com>
2021-12-14 20:41:45 +1030

    Commended apiRoutes file for easier reading.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,25 @@
+/**
+ * Required modules for Api Routes.
+ */
 const api = require('express').Router();
 const api_controller = require('../controllers/apiController');
 const { apiSecured } = require('../middlewares/auth');
 
+/**
+ * Index of our api.
+ */
 api.get('/', api_controller.index);
 
-api.post('/login', api_controller.authUser)
+/**
+ * Authentication Route Handler
+ */
+api.post('/auth/login', api_controller.authUser)
 
-api.get('/users', apiSecured, api_controller.getUsers)
+
+/**
+ * Users Route Handler
+ */
+api.get('/users/all', apiSecured, api_controller.getUsers)
 
 api.post('/users/new', apiSecured, api_controller.newUser)
 
@@ -16,6 +29,10 @@ api.post('/users/:id', apiSecured, api_controller.updateUser)
 
 api.delete('/users/:id', apiSecured, api_controller.removeUser)
 
+
+/**
+ * Role Route Handler
+ */
 api.get('/roles', apiSecured, api_controller.getRoles)
 
 api.get('/roles/:id', apiSecured, api_controller.getRole)
@@ -26,6 +43,10 @@ api.post('/roles/new', apiSecured, api_controller.newRole)
 
 api.delete('/roles/:id', apiSecured, api_controller.removeRole)
 
+
+/**
+ * Appliances Route Handler
+ */
 api.get('/appliances', apiSecured, api_controller.getAppliances)
 
 api.get('/appliances/brigade/:brigadeId', apiSecured, api_controller.getAppliancesByBrigade)
@@ -38,6 +59,10 @@ api.post('/appliances/new', apiSecured, api_controller.newAppliance)
 
 api.delete('/appliances/:id', apiSecured, api_controller.removeAppliance)
 
+
+/**
+ * ApplianceClass Route Handler
+ */
 api.get('/applianceClasses', apiSecured, api_controller.getApplianceClasses)
 
 api.get('/applianceClasses/:id', apiSecured, api_controller.getApplianceClass)
@@ -48,6 +73,10 @@ api.post('/applianceClasses/new', apiSecured, api_controller.newApplianceClass)
 
 api.delete('/applianceClasses/:id', apiSecured, api_controller.removeApplianceClass)
 
+
+/**
+ * Brigade Route Handler
+ */
 api.get('/brigades', apiSecured, api_controller.getBrigades)
 
 api.post('/brigades/new', apiSecured, api_controller.newBrigade)
@@ -58,6 +87,10 @@ api.post('/brigades/:id', apiSecured, api_controller.updateBrigade)
 
 api.delete('/brigades/:id', apiSecured, api_controller.removeBrigade)
 
+
+/**
+ * Campaign Route Handler
+ */
 api.get('/campaigns', apiSecured, api_controller.getCampaigns)
 
 api.get('/campaigns/brigade/:id', apiSecured, api_controller.getCampaignsByBrigade)
@@ -70,6 +103,10 @@ api.post('/campaigns/new', apiSecured, api_controller.newCampaign)
 
 api.delete('/campaigns/:id', apiSecured, api_controller.removeCampaign)
 
+
+/**
+ * Routes Route Handler
+ */
 api.get('/routes', apiSecured, api_controller.getRoutes)
 
 api.get('/routes/campaign/:campaignId', apiSecured, api_controller.getRoutesByCampaign)
@@ -82,4 +119,7 @@ api.post('/routes/new', apiSecured, api_controller.newRoute)
 
 api.delete('/routes/:id', apiSecured, api_controller.removeRoute)
 
+/**
+ * Export our api module.
+ */
 module.exports = api
