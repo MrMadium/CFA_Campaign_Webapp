2d70f48f5145eda9637c783c8e5e3d72891127d3
MrMadium <matt@malone.today>
2021-12-14 14:36:33 +1100

    Updated Campaign.campaignNotes to STRING to reflect VARCHAR and removed character limitation.

--- models/Campaign.js
+++ models/Campaign.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: true
         },
         campaignNotes: {
-            type: DataTypes.STRING(1) + ' CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci',
+            type: DataTypes.STRING + ' CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci',
             allowNull: true
         },
         campaignStatus: {
