b9a46fe7ddabb168a1e4ae460583cd88f47b1e08
MrMadium <matt@malone.today>
2021-12-21 21:51:48 +1100

    Cleaning up font-awesome, added map.js to work on public facing map of live position, planned route and recorded route.

---
+++ views/public/map.js
@@ -0,0 +1,16 @@
+/* IMPORT Libraries and consts, like Socket instances. */
+const map_info = require("../config/config.json");
+
+/* Create a map instance. */
+
+
+/* Bring in the database planned route geom as one layer, one colour code (probably green for Christmas.).*/
+
+
+/* Bring in the recorded instances (if any) of the route. Colour code as red. */
+
+
+/* Set Marker parameters. See if we can bring in a custom Santa face, firetruck or something like that.*/
+
+
+/* Do we add additional "float marker" information about the appliance? Appliance name? ID? */
