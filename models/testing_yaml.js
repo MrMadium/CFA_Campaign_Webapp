69106faf704a9ec5c27593ad9fb4c45010f8bfd9
MrMadium <matt@malone.today>
2021-12-23 15:54:10 +1100

    Trying to set up new database connection information using YAML for environment variables.

---
+++ models/testing_yaml.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+const yaml = require('js-yaml');
+
+/* Reading in the YAML env Variables */
+try {
+    let fileContents = fs.readFileSync(__dirname + '/../env_variables.yaml', 'utf-8');
+    let data = yaml.loadAll(fileContents);
+    let db_env = data[0][env][0];
+    console.log(db_env['SECRET']);
+} catch (e) {
+    console.log(e);
+}
+
+
+
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    config.logging = false
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+sequelize.authenticate()
+    .then(function (err) {
+        console.log(`Sequelize has established a connection to 'cfa' on Env: ${env.toUpperCase()}`);
+    })
+    .catch(function (err) {
+        console.log(`Unable to connect to the database 'cfa' on Env: ${env.toUpperCase()}:, ${err.name}`);
+    });
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+
+*/
